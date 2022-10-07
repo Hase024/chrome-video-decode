22ccfcb27c44b3992a0933efd18da8dd2d04842e
Remo Senekowitsch <contact@remlse.dev>
2022-10-07 15:50:51 +0200

    Fix copypasta

--- enable.sh
+++ enable.sh
@@ -2,7 +2,7 @@
 
 flatpak=/var/lib/flatpak/exports/share/applications/com.google.Chrome.desktop
 
-if [ -f $foo ] && ! cat $flatpak | grep Vaapi &> /dev/null
+if [ -f $flatpak ] && ! cat $flatpak | grep Vaapi &> /dev/null
 then
     sudo sed -i '/^Exec/s/com.google.Chrome/com.google.Chrome --enable-features=VaapiVideoDecoder/' $flatpak
 fi
